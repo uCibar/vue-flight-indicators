1720ddbe20dde0b6530e12d97796d8e46d7dcd0d
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2019-04-16 06:59:05 +0300

    index.js added.

---
+++ index.js
@@ -0,0 +1,7 @@
+import Airspeed from './src/Airspeed.vue'
+import Altimeter from './src/Altimeter.vue'
+import Attitude from './src/Attitude.vue'
+import Heading from './src/Heading.vue'
+import Variometer from './src/Variometer.vue'
+
+export { Airspeed, Altimeter, Attitude, Heading, Variometer }
